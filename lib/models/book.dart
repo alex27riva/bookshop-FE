d32ecb5efe2cbc1b986dccaf13b889e05fdb10c0
Alessandro Riva <alex27riva@gmail.com>
2024-03-07 12:32:21 +0100

    Books from API

--- lib/models/book.dart
+++ lib/models/book.dart
@@ -4,4 +4,12 @@ class Book {
   final String coverImageUrl;
 
   Book(this.title, this.author, this.coverImageUrl);
+
+  factory Book.fromJson(Map<String, dynamic> json) {
+    return Book(
+      json['title'] as String,
+      json['author'] as String,
+      json['cover_image_url'] as String,
+    );
+  }
 }
--- lib/screens/booklist.dart
+++ lib/screens/booklist.dart
@@ -1,17 +1,48 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:oauth_frontend/models/book.dart';
 import 'package:oauth_frontend/screens/cartscreen.dart';
 import 'package:oauth_frontend/widgets/bookItem.dart';
+import 'package:http/http.dart' as http;
 
-class BookListScreen extends StatelessWidget {
-  final List<Book> books = [
-    Book('1984', 'George Orwell',
-        'https://unlibrodaconsigliare.it/wp-content/uploads/2020/11/1984-199x300.jpg'),
-    Book('Brave New World', 'Aldous Huxley',
-        'https://i.pinimg.com/originals/d6/72/b6/d672b6e7275ca2ec2490149c57acc6b6.jpg'),
-  ];
+class BookListScreen extends StatefulWidget {
+  const BookListScreen({super.key});
 
-  BookListScreen({super.key});
+  @override
+  State<BookListScreen> createState() => _BookListScreenState();
+}
+
+class _BookListScreenState extends State<BookListScreen> {
+  List<Book> books = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<List<Book>> fetchData() async {
+    final response =
+        await http.get(Uri.parse('http://localhost:5000/api/books'));
+
+    if (response.statusCode == 200) {
+      // Parse the JSON response
+      final List<dynamic> data = jsonDecode(response.body);
+
+    return data.map((item) {
+      return Book(
+        item['title'],
+        item['author'],
+        item['cover_image_url'],
+      );
+    }).toList();
+    } else {
+      // Handle errors
+      print('Failed to load data: ${response.statusCode}');
+      return [];
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,17 +56,32 @@ class BookListScreen extends StatelessWidget {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => CartScreen(),
+                  builder: (context) => const CartScreen(),
                 ),
               );
             },
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: books.length,
-        itemBuilder: (context, index) {
-          return BookItem(book: books[index]);
+      body: FutureBuilder<List<Book>>(
+        future: fetchData(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return BookItem(book: snapshot.data![index]);
+              },
+            );
+          }
         },
       ),
     );
