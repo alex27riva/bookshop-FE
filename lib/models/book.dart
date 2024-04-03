c7619082bd426010fcede139ad429f9c3b3ca2d9
Alessandro Riva <alex27riva@gmail.com>
2024-04-03 14:44:42 +0200

    Add id and price to Book

--- lib/models/book.dart
+++ lib/models/book.dart
@@ -1,15 +1,19 @@
 class Book {
+  final int id;
   final String title;
   final String author;
+  final double price;
   final String coverImageUrl;
 
-  Book(this.title, this.author, this.coverImageUrl);
+  Book({this.id = -1, required this.title, required this.author, required this.price, required this.coverImageUrl});
 
   factory Book.fromJson(Map<String, dynamic> json) {
     return Book(
-      json['title'] as String,
-      json['author'] as String,
-      json['cover_image_url'] as String,
+      id: json['id'] as int,
+      title: json['title'] as String,
+      author: json['author'] as String,
+      price: json['price'] ?? 999.0, //high price if is null
+      coverImageUrl:  json['cover_image_url'] as String,
     );
   }
 }
