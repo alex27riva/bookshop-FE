32642322aa05157ed0ad47a824d85092afb4b030
Alessandro Riva <alex27riva@gmail.com>
2024-04-13 14:43:03 +0200

    Add border in AdminPage

--- lib/screens/admin_page.dart
+++ lib/screens/admin_page.dart
@@ -28,105 +28,134 @@ class _AdminPageState extends State<AdminPage> {
         title: const Text('Admin page'),
       ),
       drawer: const CustomSideMenu(),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10.0), // adjust border radius
-                border: Border.all(color: Colors.blue), // adjust color and width as needed
-              ),
-              padding: const EdgeInsets.all(16.0),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: "Title",
+      body: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0), // adjust border radius
+              border: Border.all(
+                  color: Colors.green), // adjust color and width as needed
+            ),
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                const Text(
+                  "Add a new book",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
+                ),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: "Title",
+                        ),
+                        validator: (value) =>
+                            value!.isEmpty ? "Please enter a title" : null,
+                        onSaved: (value) => _title = value!,
                       ),
-                      validator: (value) =>
-                          value!.isEmpty ? "Please enter a title" : null,
-                      onSaved: (value) => _title = value!,
-                    ),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: "Author",
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: "Author",
+                        ),
+                        validator: (value) =>
+                            value!.isEmpty ? "Please enter an author" : null,
+                        onSaved: (value) => _author = value!,
                       ),
-                      validator: (value) =>
-                          value!.isEmpty ? "Please enter an author" : null,
-                      onSaved: (value) => _author = value!,
-                    ),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: "Price",
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: "Price",
+                        ),
+                        keyboardType: TextInputType.number,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Please enter a price";
+                          } else {
+                            double? price = double.tryParse(value);
+                            if (price == null || price < 0) {
+                              return "Please enter a valid price";
+                            }
+                            return null;
+                          }
+                        },
+                        onSaved: (value) => _price = double.parse(value!),
                       ),
-                      keyboardType: TextInputType.number,
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return "Please enter a price";
-                        } else {
-                          double? price = double.tryParse(value);
-                          if (price == null || price < 0) {
-                            return "Please enter a valid price";
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: "Cover image url",
+                        ),
+                        keyboardType: TextInputType.url,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Please enter cover url";
                           }
                           return null;
-                        }
-                      },
-                      onSaved: (value) => _price = double.parse(value!),
-                    ),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: "Cover image url",
+                        },
+                        onSaved: (value) => _coverUrl = value!,
                       ),
-                      keyboardType: TextInputType.url,
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return "Please enter cover url";
-                        }
-                        return null;
-                      },
-                      onSaved: (value) => _coverUrl = value!,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          _formKey.currentState!.save();
-                          BackendService.adminBookAdd(
-                              _title, _author, _price, _coverUrl);
-                        }
-                      },
-                      child: const Text("Add Book"),
-                    ),
-                  ],
+                      const SizedBox(height: 10),
+                      ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            _formKey.currentState!.save();
+                            BackendService.adminBookAdd(
+                                _title, _author, _price, _coverUrl);
+                          }
+                        },
+                        child: const Text("Add Book"),
+                      ),
+                    ],
+                  ),
                 ),
+              ],
+            ),
+          ),
+          SingleChildScrollView(
+            child: Container(
+              margin: const EdgeInsets.only(top: 10.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                // adjust border radius
+                border: Border.all(
+                    color:
+                        Colors.redAccent), // adjust color and width as needed
+              ),
+              child: Column(
+                children: [
+                  const Text(
+                    "Remove a book",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
+                  ),
+                  FutureBuilder<List<Book>>(
+                    future: BackendService.fetchBooks(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(child: CircularProgressIndicator());
+                      } else if (snapshot.hasError) {
+                        return Center(child: Text('Error: ${snapshot.error}'));
+                      } else {
+                        books = snapshot.data!;
+                        return ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: books.length,
+                            itemBuilder: (context, index) {
+                              final book = books[index];
+                              return AdminListTile(
+                                book: book,
+                                onDelete: () => _handleDeleteBook(book.id),
+                              );
+                            });
+                      }
+                    },
+                  ),
+                ],
               ),
             ),
-            FutureBuilder<List<Book>>(
-              future: BackendService.fetchBooks(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasError) {
-                  return Center(child: Text('Error: ${snapshot.error}'));
-                } else {
-                  books = snapshot.data!;
-                  return ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: books.length,
-                      itemBuilder: (context, index) {
-                        final book = books[index];
-                        return AdminListTile(
-                          book: book,
-                          onDelete: () => _handleDeleteBook(book.id),
-                        );
-                      });
-                }
-              },
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
