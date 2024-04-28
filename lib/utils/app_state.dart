c06e23eae179dbbb62818edb3dfd18d22bad2a89
Alessandro Riva <alex27riva@gmail.com>
2024-04-28 11:21:15 +0200

    Implement GoRoute for OIDC

---
+++ lib/utils/app_state.dart
@@ -0,0 +1,69 @@
+// ignore_for_file: avoid_redundant_argument_values
+
+import 'package:async/async.dart';
+import 'package:bdaya_shared_value/bdaya_shared_value.dart';
+import 'package:logging/logging.dart';
+import 'package:oidc/oidc.dart';
+import 'package:oidc_default_store/oidc_default_store.dart';
+
+//This file represents a global state, which is bad
+//in a production app (since you can't test it).
+
+//usually you would use a dependency injection library
+//and put these in a service.
+final exampleLogger = Logger('oidc.example');
+
+/// Gets the current manager used in the example.
+OidcUserManager currentManager = duendeManager;
+
+final duendeManager = OidcUserManager.lazy(
+  discoveryDocumentUri: OidcUtils.getOpenIdConfigWellKnownUri(
+    Uri.parse('http://localhost:8080/realms/unimi'),
+  ),
+  // this is a public client,
+  // so we use [OidcClientAuthentication.none] constructor.
+  clientCredentials: const OidcClientAuthentication.none(
+    clientId: 'flutter',
+  ),
+  store: OidcDefaultStore(
+      // useSessionStorageForSessionNamespaceOnWeb: true,
+      ),
+
+  // keyStore: JsonWebKeyStore(),
+  settings: OidcUserManagerSettings(
+    frontChannelLogoutUri: Uri(path: 'redirect.html'),
+    uiLocales: ['ar'],
+    refreshBefore: (token) {
+      return const Duration(seconds: 1);
+    },
+
+    // scopes supported by the provider and needed by the client.
+    scope: ['openid', 'profile', 'email', 'offline_access'],
+    postLogoutRedirectUri: Uri.parse('http://localhost:8000/redirect.html'),
+
+    redirectUri:
+        // this url must be an actual html page.
+        // see the file in /web/redirect.html for an example.
+        //
+        // for debugging in flutter, you must run this app with --web-port 8000
+        Uri.parse('http://localhost:8000/redirect.html'),
+  ),
+);
+
+final initMemoizer = AsyncMemoizer<void>();
+
+Future<void> initApp() {
+  return initMemoizer.runOnce(() async {
+    // String? _platformName;
+    currentManager.userChanges().listen((event) {
+      cachedAuthedUser.$ = event;
+      exampleLogger.info(
+        'User changed: ${event?.claims.toJson()}, info: ${event?.userInfo}',
+      );
+    });
+
+    await currentManager.init();
+  });
+}
+
+final cachedAuthedUser = SharedValue<OidcUser?>(value: null);
